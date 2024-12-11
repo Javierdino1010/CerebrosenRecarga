ccb8254cc65e9591f6951a44c2859ea5c4171e08
ikasle <ikasle@DAM2-16.arangoya.local>
2024-12-11 09:54:04 +0100

    Ligeros cambios

--- TrabajoAritz/src/vista/Login.java
+++ TrabajoAritz/src/vista/Login.java
@@ -13,25 +13,6 @@ public class Login extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Login frame = new Login();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
 	public Login() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
